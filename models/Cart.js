f67976409b05678ee0800dd45f7587626833a083
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-12-08 11:13:48 -0300

    Create Cart model in models file

---
+++ models/Cart.js
@@ -0,0 +1,62 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Cart = sequelize.define('Cart', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'users',
+      key: 'id'
+    }
+  },
+  status: {
+    type: DataTypes.ENUM('active', 'abandoned', 'converted'),
+    defaultValue: 'active'
+  }
+}, {
+  tableName: 'carts',
+  timestamps: true
+});
+
+const CartItem = sequelize.define('CartItem', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  cartId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'carts',
+      key: 'id'
+    }
+  },
+  productId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'products',
+      key: 'id'
+    }
+  },
+  quantity: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 1,
+    validate: {
+      min: 1
+    }
+  }
+}, {
+  tableName: 'cart_items',
+  timestamps: true
+});
+
+module.exports = { Cart, CartItem };
