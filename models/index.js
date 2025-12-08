fa25f07a006978675ae95107e86c8d7c92da8586
Patricio Andrade Gallardo <patricioandradeh@gmail.com>
2025-12-08 19:08:48 -0300

    Refactor: Relaciones actualizadas

--- models/index.js
+++ models/index.js
@@ -1,8 +1,12 @@
 const User = require('./User');
 const Product = require('./Product');
 const { Order, OrderItem } = require('./Order');
+const { Cart, CartItem } = require('./Cart');
+const Review = require('./Review');
+const Coupon = require('./Coupon');
+const Wishlist = require('./Wishlist');
 
-// Relaciones
+// Relaciones de Order
 User.hasMany(Order, { foreignKey: 'userId', as: 'orders' });
 Order.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
@@ -12,9 +16,38 @@ OrderItem.belongsTo(Order, { foreignKey: 'orderId' });
 Product.hasMany(OrderItem, { foreignKey: 'productId' });
 OrderItem.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
 
+// Relaciones de Cart
+User.hasOne(Cart, { foreignKey: 'userId', as: 'cart' });
+Cart.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+Cart.hasMany(CartItem, { foreignKey: 'cartId', as: 'items' });
+CartItem.belongsTo(Cart, { foreignKey: 'cartId' });
+
+Product.hasMany(CartItem, { foreignKey: 'productId' });
+CartItem.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
+
+// Relaciones de Review
+User.hasMany(Review, { foreignKey: 'userId', as: 'reviews' });
+Review.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+Product.hasMany(Review, { foreignKey: 'productId', as: 'reviews' });
+Review.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
+
+// Relaciones de Wishlist
+User.hasMany(Wishlist, { foreignKey: 'userId', as: 'wishlist' });
+Wishlist.belongsTo(User, { foreignKey: 'userId', as: 'user' });
+
+Product.hasMany(Wishlist, { foreignKey: 'productId' });
+Wishlist.belongsTo(Product, { foreignKey: 'productId', as: 'product' });
+
 module.exports = {
   User,
   Product,
   Order,
-  OrderItem
+  OrderItem,
+  Cart,
+  CartItem,
+  Review,
+  Coupon,
+  Wishlist
 };
